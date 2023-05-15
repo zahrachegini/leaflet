ebca163612b51d577c9881ce7e056826727cb803
Zahra Chegini <zch6511@gmail.com>
2023-05-15 18:19:53 +0330

    Set position

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -91,7 +91,7 @@ const SearchBox = (props) => {
               return (
                 <li
                   key={item?.place_id}
-                  className="border border-2 border-gray-400/60 bg-white p-1 cursor-pointer"
+                  className="border border-2 border-gray-400/60 bg-white p-1 cursor-pointer text-right"
                   onClick={() => {
                     console.log(item.lat, item.lon);
                     setSelectPosition(item);
